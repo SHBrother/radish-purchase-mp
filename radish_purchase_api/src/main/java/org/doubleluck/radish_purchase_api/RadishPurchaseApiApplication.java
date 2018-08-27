030129df3a39acf3050029d2453b6b98b1ce7adb
lzf <doubleluck.li@shein.com>
2018-08-27 16:12:49 +0800

    add annotation on main class

--- radish_purchase_api/src/main/java/org/doubleluck/radish_purchase_api/RadishPurchaseApiApplication.java
+++ radish_purchase_api/src/main/java/org/doubleluck/radish_purchase_api/RadishPurchaseApiApplication.java
@@ -1,9 +1,13 @@
 package org.doubleluck.radish_purchase_api;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@MapperScan("org.doubleluck.radish_purchase_api.repository")
+@ServletComponentScan
 public class RadishPurchaseApiApplication {
 
     public static void main(String[] args) {
--- radish_purchase_api/src/test/java/org/doubleluck/radish_purchase_api/service/impl/GoodsServiceImplTest.java
+++
@@ -1,7 +0,0 @@
-package org.doubleluck.radish_purchase_api.service.impl;
-
-import static org.junit.Assert.*;
-
-public class GoodsServiceImplTest {
-
-}
